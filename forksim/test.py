a54fb91fca5a0f7bb7512b773919092b901404a1
Nchlt <nchlltaf@gmail.com>
2018-02-02 13:14:23 +0100

    update

--- forksim/test.py
+++ forksim/test.py
@@ -3,6 +3,7 @@ import hashlib
 nb_node = 0
 initial_chain = []
 difficulty = "00";
+proof_size = 32;
 
 class Block:
     def __init__(self, bid, previousHash, hash):
@@ -20,16 +21,27 @@ class Node:
     def whoAmI(self):
         print("I'm node %d"%self.nid)
 
-    def checkProof(proof, block):
-        global difficulty
-        return 0
+    # Renvoie vrai si la preuve permet de miner le bloc
+    def checkHash(hash, block):
+        if (hash.startswith(difficulty)):
+            return True
+        else:
+            return False
 
+    # Renvoie le hash d'une tentative de minage du bloc block
     def computeProof(block):
+        global proof_size
+        sha = hashlib.sha256()
+        nonce = ''.join(random.SystemRandom().choice(string.ascii_uppercase +
+                             string.digits +
+                             string.ascii_lowercase)  for _ in range(proof_size))
 
-        while not (sha2.hexdigest().startswith(file_zeros)):
-            mes =  proof_old + (''.join(random.SystemRandom().choice(string.ascii_uppercase +
-                                                       string.digits +
-                                                   string.ascii_lowercase)  for _ in range(p_size))
+        toHash = block.previousHash + nonce
+        sha.update(toHash)
+        return sha.hexdigest()
 
 
 nodes = [Node(nb_node, initial_chain) for _ in range(10)]
+
+for node in nodes:
+    node.whoAmI()
